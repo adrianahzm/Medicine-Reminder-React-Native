237d060a09f65d12d6d53c72752b9d965fd0fcf1
adrianahzm <adriiana.guzman@hotmail.com>
2025-02-17 23:26:42 -0600

    creación de auth

---
+++ app/auth.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { useRouter } from "expo-router";
+import * as LocalAuthentication from "expo-local-authentication";
+import { Ionicons } from "@expo/vector-icons";
+// import { LinearGradient } from "expo-linear-gradient";
---
+++ app/index.tsx
@@ -0,0 +1,82 @@
+import { View, Text, StyleSheet, Animated } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect, useRef } from "react";
+import { useRouter } from "expo-router";
+
+export default function SplashScreen() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+  const router = useRouter();
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        useNativeDriver: true,
+      }),
+    ]).start();
+
+    const timer = setTimeout(() => {
+      router.replace("/auth");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.iconContainer,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Ionicons name="logo-react" size={32} color="#fff" />
+        <Text style={styles.appName}> MedicRemind</Text>
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#11263d",
+  },
+  iconContainer: {
+    color: "#fff",
+    alignItems: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  appName: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 20,
+    letterSpacing: 1,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
