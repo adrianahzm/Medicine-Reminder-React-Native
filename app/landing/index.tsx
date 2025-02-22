06a1ca3399c9d057d7027a48883ac7f44d236dbd
adrianahzm <adriiana.guzman@hotmail.com>
2025-02-22 15:29:56 -0600

    estructura landing

---
+++ app/landing/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Landing() {
+    return (
+        <View>
+            <Text>Bienvenido a la Landing Page</Text>
+        </View>
+    );
+}
